73ee87240e5c3824a92f240d1a8b7f8ecd184d80
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-09-06 21:53:45 +0300

    update

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,4 +1,25 @@
 const router=require('express').Router()
 const userModel=require('../models/user')
 
-//get user
+router.post('/add',async(req,res)=>{
+    try{
+   const data =req.body
+   const newBook=new userModel(data)
+   await newBook.save().then(()=>{
+       res.status(200).json({message:"User addedsucccessfuly"})
+   })
+    }
+    catch(error){
+    console.log(error)
+    }
+   })
+   ///getrequest
+   router.get("/getUsers",async(req,res)=>{
+   try{
+   books=await userModel.find()
+   res.status(200).json({user})
+   }
+   catch(error){
+       console.log(error)
+   }
+   })
